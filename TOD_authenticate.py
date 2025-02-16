f6dacf3880c9a9acac41a3a6cf9abe81ca5af706
sujit27 <sujit271290@gmail.com>
2025-02-15 20:37:38 -0500

    runnning authentication file with prompt separated

--- TOD_authenticate.py
+++ TOD_authenticate.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-
-
 import uuid
 import getpass
 import os
@@ -17,6 +15,8 @@ from typing import List, Literal, Annotated
 from langgraph.graph import StateGraph, START, END
 from langgraph.checkpoint.memory import MemorySaver
 
+from prompt import prompt_system_task, prompt_authenticate
+
 
 os.environ["OPENAI_API_KEY"] = "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"
 os.environ["LANGSMITH_API_KEY"] = "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"
@@ -25,35 +25,21 @@ os.environ["LANGCHAIN_TRACING_V2"] = "true"
 os.environ["LANGCHAIN_PROJECT"] = "pr-authorized-someplace-95"
 
 
-
-
 class StateSchema(TypedDict):
     messages: Annotated[list, add_messages]
 
+class AuthenticationProfile(BaseModel):
+    """Information about authentication fields"""
+    policy_number: str = Field(default=None, description="The policy number of the user")
+    last_name: str = Field(default=None, description="The last name of the user")
+    date_of_birth: str = Field(default=None, description="The date of birth of the user")
 
-
-
-prompt_system_task = """Your job is to gather information from the user and authenticate.
-
-You should obtain the following authentication fields from them:
-
-1. policy number
-2. last name
-3. date of birth
-
-Ask the user for authentication fields. 
-If the use does not provide value for a field after repeated requests, let them know you would not be able to proceed further without it.
-If the use tries to engage them in any other conversation, bring them to the task of authenticating themselves in a polite and humorous way.
-
-ONLY OFTER you are able to get all the 3 authentication fields from the user, call the relevant tool."""
-
-
-
+class ResponseFormatter(BaseModel):
+    """Always use this tool to structure the output"""
+    answer: bool = Field(description="True or False depending if user authentication was successful or not")
 
 def domain_state_tracker(messages):
     return [SystemMessage(content=prompt_system_task)] + messages
-
-
 
 llm = ChatOpenAI(
     model="gpt-4o",
@@ -63,37 +49,10 @@ llm = ChatOpenAI(
     max_retries=2,
 )
 
-
-
-class AuthenticationProfile(BaseModel):
-    """Information about authentication fields"""
-    policy_number: str = Field(default=None, description="The policy number of the user")
-    last_name: str = Field(default=None, description="The last name of the user")
-    date_of_birth: str = Field(default=None, description="The date of birth of the user")
-
-
-
-class ResponseFormatter(BaseModel):
-    """Always use this tool to structure the output"""
-    answer: bool = Field(description="True or False depending if user authentication was successful or not")
-
-
-
 user_info = {"policy_number":"0123456789","last_name":"Sahoo","date_of_birth":"27 Dec 1990"}
 
-
-
 llm_to_collect_info = llm.bind_tools([AuthenticationProfile])
-
-
-
 llm_to_authenticate = llm.bind_tools([ResponseFormatter])
-
-
-
-workflow = StateGraph(StateSchema)
-
-
 
 def call_llm(state: StateSchema):
     """
@@ -103,17 +62,6 @@ def call_llm(state: StateSchema):
     messages = domain_state_tracker(state["messages"])
     response = llm_to_collect_info.invoke(messages)
     return {"messages": [response]}
-
-
-
-
-workflow.add_node("talk_to_user", call_llm)
-
-
-
-workflow.add_edge(START, "talk_to_user")
-
-
 
 def finalize_dialogue(state: StateSchema):
     """
@@ -128,19 +76,6 @@ def finalize_dialogue(state: StateSchema):
         ]
     }
 
-workflow.add_node("finalize_dialogue", finalize_dialogue)
-
-
-
-prompt_generate_user_story = """Based on the following extracted fields from the user and the actual field values from database,/
- authenticate the user by providing True or False in output:
-
-extracted fields: {reqs}
-
-actual fields: {user_info}
-
-"""
-
 def build_prompt_to_generate_user_story(messages: list):
     tool_call = None
     other_msgs = []
@@ -151,17 +86,13 @@ def build_prompt_to_generate_user_story(messages: list):
             continue
         elif tool_call is not None:
             other_msgs.append(m)
-    return [SystemMessage(content=prompt_generate_user_story.format(reqs=tool_call,user_info=user_info))] + other_msgs
+    return [SystemMessage(content=prompt_authenticate.format(reqs=tool_call,user_info=user_info))] + other_msgs
 
 
 def call_model_to_generate_user_story(state):
     messages = build_prompt_to_generate_user_story(state["messages"])
     response = llm_to_authenticate.invoke(messages)
     return {"messages": [response]}
-
-workflow.add_node("authenticate_user", call_model_to_generate_user_story)
-
-
 
 def define_next_action(state) -> Literal["finalize_dialogue", END]:
     messages = state["messages"]
@@ -171,14 +102,14 @@ def define_next_action(state) -> Literal["finalize_dialogue", END]:
     else:
         return END
 
+workflow = StateGraph(StateSchema)
+workflow.add_node("talk_to_user", call_llm)
+workflow.add_edge(START, "talk_to_user")
+workflow.add_node("finalize_dialogue", finalize_dialogue)
+workflow.add_node("authenticate_user", call_model_to_generate_user_story)
 workflow.add_conditional_edges("talk_to_user", define_next_action)
-
-
-
 workflow.add_edge("finalize_dialogue", "authenticate_user")
 workflow.add_edge("authenticate_user", END)
-
-
 
 memory = MemorySaver()
 graph = workflow.compile(checkpointer=memory)
--- prompt.py
+++ prompt.py
@@ -2,13 +2,21 @@ prompt_system_task = """Your job is to gather information from the user and auth
 
 You should obtain the following authentication fields from them:
 
-1. policy number: acceptable policy number provided by user should be 0123456789 with no variations
-2. full name: acceptable full name provided by user should be John Doe, minor variations like Jon Doe or John Dow are acceptable
-3. date of birth: acceptable date of birth provided by user should be Dec 27 1990
+1. policy number
+2. last name
+3. date of birth
 
-Ask the user and check authentication field one by one. DO NOT preemptively provide the acceptable value for the fields.
-DO NOT ask for a field which has already been authenticated.
-If value provided by user does not match, let the user know that the value provided does not match the acceptable value on file, DO NOT move to the next field.
+Ask the user for authentication fields. 
+If the use does not provide value for a field after repeated requests, let them know you would not be able to proceed further without it.
 If the use tries to engage them in any other conversation, bring them to the task of authenticating themselves in a polite and humorous way.
 
-After you are able to get all the information from the user, call the relevant tool."""
+ONLY OFTER you are able to get all the 3 authentication fields from the user, call the relevant tool."""
+
+prompt_authenticate = """Based on the following extracted fields from the user and the actual field values from database,/
+ authenticate the user by providing True or False in output:
+
+extracted fields: {reqs}
+
+actual fields: {user_info}
+
+"""
